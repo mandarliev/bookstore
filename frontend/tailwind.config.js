bc514a4a74283082b5aca761ab03d83512bc62e1
Dimitar <dimitar.mandarliev@gmail.com>
2024-12-22 22:05:40 +0200

    customizing project

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FFCE1A",
+        secodary: "#0D0842",
+        blackBg: "#F3F3F3",
+        Favorite: "#FF5841",
+      },
+      fontFamily: {
+        primary: ["Montserrat", "serif"],
+        secondary: ["Nunito Sans", "serif"],
+      },
+    },
   },
   plugins: [],
 };
