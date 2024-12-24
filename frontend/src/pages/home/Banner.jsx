73769a43a9a21aaf09378325e00793251b29ef46
Dimitar <dimitar.mandarliev@gmail.com>
2024-12-24 18:23:46 +0200

    fixed positioning of banner

--- frontend/src/pages/home/Banner.jsx
+++ frontend/src/pages/home/Banner.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import bannerImg from "../../assets/banner.png";
+import bannerImg from "../../assets/banner.jpg";
 
 const Banner = () => {
   return (
-    <div className="flex flex-col md:flex-row py-16 justify-between items-center gap-12">
+    <div className="flex flex-col md:flex-row-reverse py-16 justify-between items-center gap-12">
+      <div className="md:w-1/2 w-full flex items-center md:justify-end">
+        <img src={bannerImg} />
+      </div>
       <div className="md:w-1/2 w-full">
         <h1 className="md:text-5xl text-2xl font-medium mb-7">Releases</h1>
         <p className="mb-10">
@@ -13,9 +16,6 @@ const Banner = () => {
           these releases offer captivating philosophy and economics teachings.{" "}
         </p>
         <button className="btn-primary">Subscribe</button>
-      </div>
-      <div className="md:w-1/2 w-full flex items-center md:justify-end">
-        <img src={bannerImg} />
       </div>
     </div>
   );
